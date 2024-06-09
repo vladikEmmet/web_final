f913ccd038367031f999d519986769b6eb958029
Obedkov Vladislav <230765@astanait.edu.kz>
2024-06-10 00:57:06 +0500

    I hope its last commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,27 +1,27 @@
-const { override, addWebpackModuleRule } = require('customize-cra');
-
-module.exports = override(
-    addWebpackModuleRule({
-        test: /\.scss$/,
-        use: [
-            {
-                loader: "style-loader"
-            },
-            {
-                loader: "css-loader"
-            },
-            {
-                loader: "resolve-url-loader",
-                options: {
-                    sourceMap: true // Включение source maps для resolve-url-loader
-                }
-            },
-            {
-                loader: "sass-loader",
-                options: {
-                    sourceMap: true // Включение source maps для sass-loader
-                }
-            }
-        ]
-    })
-);
+// const { override, addWebpackModuleRule } = require('customize-cra');
+//
+// module.exports = override(
+//     addWebpackModuleRule({
+//         test: /\.scss$/,
+//         use: [
+//             {
+//                 loader: "style-loader"
+//             },
+//             {
+//                 loader: "css-loader"
+//             },
+//             {
+//                 loader: "resolve-url-loader",
+//                 options: {
+//                     sourceMap: true // Включение source maps для resolve-url-loader
+//                 }
+//             },
+//             {
+//                 loader: "sass-loader",
+//                 options: {
+//                     sourceMap: true // Включение source maps для sass-loader
+//                 }
+//             }
+//         ]
+//     })
+// );
