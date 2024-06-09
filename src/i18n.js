dcdcbc650858d938abd4f100a87fd110eb219e56
Obedkov Vladislav <230765@astanait.edu.kz>
2024-06-10 00:30:54 +0500

    Changed i18n

--- src/i18n.js
+++ src/i18n.js
@@ -25,6 +25,9 @@ i18n
 
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
+        },
+        backend: {
+            loadPath: '/web_final/locales/{{lng}}/{{ns}}.json'
         }
     });
 
